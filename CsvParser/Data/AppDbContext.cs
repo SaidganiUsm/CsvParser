bb63d62772d9b0fee9c5c6b7d6e45ea4cadbfe57
SaidganiUsm <saidgani110@gmail.com>
2023-07-29 21:13:08 +0500

    Migration process is done, now turn for api and its logic

--- CsvParser/Data/AppDbContext.cs
+++ CsvParser/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace CsvParser.Data
+﻿using CsvParser.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CsvParser.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
--- CsvParser/Models/User.cs
+++ CsvParser/Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace CsvParser.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsvParser.Models
 {
     public class User
     {
+        [Key]
+        public string UserIdentifier { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
